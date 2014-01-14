71525ac58c02d6d3b9feae515fc869445f51f4c7
JuliePO <pojulie07@gmail.com>
2014-01-14 12:16:31 +0100

    commit test + changement mail admin dans setting.py

--- komuso/apps/core/views.py
+++ komuso/apps/core/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 def home(request):
   text = """<h1>Application komuso !</h1>
-            <p>page de test</p>"""
+            <p>page de test (faut qu'on change de nom)</p>"""
   return HttpResponse(text)
