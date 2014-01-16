7604ba51cd4e3a4137c61d35e9ebb41850b014e2
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-01-16 15:17:28 +0100

    suppression fichiers inutiles

--- komuso/apps/core/views.py
+++
@@ -1,7 +0,0 @@
-#-*- coding: utf-8 -*-
-from django.http import HttpResponse
-
-def home(request):
-  text = """<h1>Application komuso !</h1>
-            <p>page de test (faut qu'on change de nom)</p>"""
-  return HttpResponse(text)
