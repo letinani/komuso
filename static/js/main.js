75828c2e8069879d45eb69b1aa2a183675335e47
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-01-23 17:06:52 +0100

    gestion des event clics sur menu coulissant et menu des notes

--- static/js/main.js
+++ static/js/main.js
@@ -10,10 +10,43 @@ $(function() {
         return false;
     });
 
+    $(document).click(function(e) {
+        if (!$(e.target).is("#side-menu")) {
+            if ($('#side-menu').is(':visible')) {
+                $("#side-menu").removeClass('open');
+            }
+        }
+    });
+
+
+
+
+    // menu ajout des notes
     $(".menu-notes a").click(function(e){
         $(".menu-sub").addClass('open');
         return false;
     });
 
-    
+
+   // On cache les sous-menus :
+    $("#menu-notes .menu-sub").hide();
+
+
+    // On modifie l'évènement "click" sur les liens dans les items de liste
+    // qui portent la classe "toggleSubMenu" :
+    $("a.note-picker").click( function () {
+        // Si le sous-menu était déjà ouvert, on le referme :
+        if ($(this).next(".menu-sub:visible").length != 0) {
+            $(this).next().hide();
+        }
+        // Si le sous-menu est caché, on ferme les autres et on l'affiche :
+        else {
+            $("#menu-notes .menu-sub").hide();
+            $(this).next().show();
+        }
+        // On empêche le navigateur de suivre le lien :
+        return false;
+    });
+
+
 });
