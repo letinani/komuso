18ff504ec66ed0f581fd7050f70490f9ff62eee4
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-01-23 15:39:11 +0100

    resolution conflit + style de la page

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+
+    // menu coulissant
     $(".open-side-menu").click(function(e){
     	$("#side-menu").addClass('open');
     	return false;
@@ -7,4 +9,11 @@ $(function() {
         $("#side-menu").removeClass('open');
         return false;
     });
+
+    $(".menu-notes a").click(function(e){
+        $(".menu-sub").addClass('open');
+        return false;
+    });
+
+    
 });
