17fa28e60349073f8ada1e940d9acbeb6ed832f4
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-04-04 16:53:06 +0200

    urls pdf commented

--- score-editor/urls.py
+++ score-editor/urls.py
@@ -4,6 +4,6 @@ import os
 
 urlpatterns = patterns('score-editor.views',
     url(r'^$', 'home'),
-    url(r'^export-pdf/', 'home_pdf'),
+    #url(r'^export-pdf/', 'home_pdf'),
     #url(r'^export-pdf/', PDFTemplateView.as_view(template_name='score-editor/index.html', filename='partition.pdf'), name='pdf'),
 )
