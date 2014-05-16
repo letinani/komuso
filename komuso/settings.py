4a3901f0e633769d2e43fee8740214f54d6f1f56
LaetitiaNanni <laetitia.nanni@gmail.com>
2014-05-16 11:11:42 +0200

    corrections diverses

--- komuso/settings.py
+++ komuso/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = '4+_55w=d2&&v$^fs7gns7(d9q#9f_wtsz-7rf!juyi1vc!v7d8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = bool(os.environ.get('DEBUG', False))
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
